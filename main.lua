e0b732b6a5632a5d8fc005978a6b71daef8d1ba7
Aurora Aquir <AuroraKy@users.noreply.github.com>
2024-05-04 18:25:46 +0200

    Create empty mod file

---
+++ main.lua
@@ -0,0 +1,16 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Celeste Card Collection
+--- MOD_ID: CelesteCardCollection
+--- MOD_AUTHOR: [AuroraAquir]
+--- MOD_DESCRIPTION: nothing for now
+--- PRIORITY: 0
+--- DISPLAY_NAME: CelesteCC
+--- BADGE_COLOUR: ffc0ff
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+
+
+----------------------------------------------
+------------MOD CODE END----------------------
