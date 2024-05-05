09256235900bbeadfd5460acb8de1d1ccdd743c8
Aurora Aquir <AuroraKy@users.noreply.github.com>
2024-05-05 13:08:08 +0200

    Added Summit Deck, update readme

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ A balatro mod using steamodded revolving around a celeste card theme
 
 # Decks
 Virus Deck: Each played card is retriggered then debuffed until the end of the ante
+Summit Deck: Start with 1 Joker Slot. Each Ante gain 1 Joker Slot (if you haven't been in this ante yet)
 # Jokers
-None
+Seeker: Gives Hang Man if Blind is selected with $4 or less
+Feather: X2 Mult when 0 discards
+Bird: Whenever a Planet card is used, draw 3 cards
 # Blinds
 None
--- decks.lua
+++ decks.lua
@@ -4,7 +4,29 @@
 ----------------------------------------------
 ------------MOD CODE--------------------------
 
------------ VIRUS DECK -----------------------
+----------- Definitions ----------------------
+
+local virus_def = {
+	["name"]="Virus Deck",
+	["text"]={
+		[1]="Each played card is retriggered",
+		[2]="then {C:red}debuffed{}",
+		[3]="until the end of the ante",
+	},
+}
+
+local summit_def = {
+	["name"]="Summit Deck",
+	["text"]={
+		[1]="Start with {C:attention}1{} Joker Slot",
+		[2]="{C:attention}+1{} Joker slot each Ante",
+		[3]="{s:0.75}(if Ante has not been reached before){}"
+	},
+}
+
+local trigger_effect_callbacks = {}
+
+-- region virus deck -----------------------
 
 local eval_cardRef = eval_card
 function eval_card(card, context) 
@@ -36,9 +58,7 @@ function Blind.debuff_card(self, card, from_blind)
 	debuff_cardRef(self, card, from_blind)
 end
 
-local trigger_effectRef = Back.trigger_effect
-
-function Back.trigger_effect(self, args)
+function virus_effect(self, args)
     if G.GAME.selected_back.effect.config.virus and args.context == 'final_scoring_step' then
 		
         G.E_MANAGER:add_event(Event({
@@ -52,23 +72,57 @@ function Back.trigger_effect(self, args)
 			end)
 		}))
 	end
-
-	return trigger_effectRef(self, args)
 end
 
-local virus_def = {
-	["name"]="Virus Deck",
-	["text"]={
-		[1]="Each played card is retriggered",
-		[2]="then {C:attention}debuffed{}",
-		[3]="until the end of the ante",
-	},
-}
+table.insert(trigger_effect_callbacks, virus_effect)
 
 local virus = SMODS.Deck:new("Virus Deck", "virus", {virus = true, atlas= "b_cccdecks"}, {x = 0, y = 0}, virus_def)
 virus:register()
 
------------ VIRUS DECK END -----------------------
+-- endregion virus deck-----------------------
+-- region summit deck -----------------------
+
+function summit_effect(self, args)
+	if self.effect.config.add_slot_each_ante and G.GAME.round_resets.ante > self.effect.config.add_slot_each_ante and args.context == 'eval' and G.GAME.last_blind and G.GAME.last_blind.boss then
+		G.E_MANAGER:add_event(Event({func = function()
+			if G.jokers then
+				G.jokers.config.card_limit = G.jokers.config.card_limit + 1
+				self.effect.config.add_slot_each_ante = self.effect.config.add_slot_each_ante + 1
+
+				local width = G.round_eval.T.w - 0.51
+				local slotAdded = {n=G.UIT.R, config={align = "cm", minw = width}, nodes={
+					{n=G.UIT.O, config={object = DynaText({string = {'+1 Joker Slot!'}, colours = {G.C.ORANGE},shadow = true, float = false, y_offset = 0, scale = 0.66, spacing = 0, font = G.LANGUAGES['en-us'].font, pop_in = 0})}}
+				}}
+				local spacer = {n=G.UIT.R, config={align = "cm", minw = width}, nodes={
+					{n=G.UIT.O, config={object = DynaText({string = {'......................................'}, colours = {G.C.WHITE},shadow = true, float = true, y_offset = -30, scale = 0.45, spacing = 13.5, font = G.LANGUAGES['en-us'].font, pop_in = 0})}}
+				}}
+				G.round_eval:add_child(spacer,G.round_eval:get_UIE_by_ID('base_round_eval'))
+				G.round_eval:add_child(slotAdded,G.round_eval:get_UIE_by_ID('base_round_eval'))
+                        
+			end
+			return true end }))
+    end
+end
+
+table.insert(trigger_effect_callbacks, summit_effect)
+
+local summit = SMODS.Deck:new("Summit Deck", "summit", {joker_slot = -4, add_slot_each_ante = 1, atlas= "b_cccdecks"}, {x = 1, y = 0}, summit_def)
+summit:register()
+
+-- endregion summit deck-----------------------
+
+-- region HOOKS
+
+local trigger_effectRef = Back.trigger_effect
+
+function Back.trigger_effect(self, args)
+	for _, callback in ipairs(trigger_effect_callbacks) do
+		callback(self, args)
+	end
+	return trigger_effectRef(self, args)
+end
+
+-- endregion HOOKS
 
 sendDebugMessage("[CCC] Decks loaded")
 ----------------------------------------------
